23b1ebcb383bdc8349a77859f340fc9da1872af1
Kaitou Doraluxe <44762852+KaitouDoraluxe@users.noreply.github.com>
2021-02-19 03:04:35 +0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,78 @@
+print("Assalam Alaikum")
+print("This is Islamic game test")
+print("Let's get started")
+
+while True:
+    # Question 1
+    choice = input("Who was the fourth Khalifa? ")
+
+    # Correct Answers
+    if choice in ('Ali', 'Ali (ra)', 'ali', 'Ali ra', 'Ali RA'):
+        
+
+        if choice == 'Ali (ra)':
+            print("Correct!")
+        
+        elif choice == 'ali':
+            print("Correct!")
+        
+        elif choice == 'Ali ra':
+            print("Correct!")
+        
+        elif choice == 'Ali RA':
+            print("Correct!")
+
+        elif choice == 'Ali':
+            print("Correct!")    
+
+                 
+
+        break
+    else:
+        print("Wrong! It was Ali (ra) Let's try again")
+
+yes = input("Another Quiz! If you want to do another Quiz, type 'yes' ")
+
+while True: 
+  # Question 2
+    question2 = input("How many rakats in Fatiha? ")
+
+    # Correct Answer
+    if question2 in ('None', 'none'):
+        
+
+        if question2 == 'None':
+            print("Correct! Because Faitha is a Surah and it is the first Surah of the Quran")
+
+        elif question2 == 'none':
+            print("Correct! Because Faitha is a Surah and it is the first Surah of the Quran")
+
+        break
+    else:
+        print("Wrong Answer! Try again!")
+
+yes = input("Another Quiz! If you want to do another Quiz, type 'yes' ")        
+
+while True: 
+  # Question 3
+    question3 = input("When prophet Muhammad [saw] was born? ")
+
+    # Correct Answer
+    if question3 in ('570 AD', '570', '570 ad'):
+        
+
+        if question3 == '570 AD':
+            print("Correct!")
+
+        elif question3 == '570':
+            print("Correct!")
+            
+        elif question3 == '570 ad':
+            print("Correct!")
+        break
+    else:
+        print("Wrong Answer! Try again!")
+
+print("Now that you answered All 3 questin correctly, heres a chocolate chip cookies for reward")
+print("https://i.redd.it/i5hupc107fh61.jpg")
+print("Created by Kaitou Doraluxe")
